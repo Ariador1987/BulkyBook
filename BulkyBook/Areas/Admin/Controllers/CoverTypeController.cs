1a855adb2829a5e65f73316b83239d369346e02e
Božidar Bralić <bozidar.bralic@gmail.com>
2021-06-28 11:42:53 +0200

    Completed CoverType Controller, last feature added HttpDelete Api Call

--- BulkyBook/Areas/Admin/Controllers/CoverTypeController.cs
+++ BulkyBook/Areas/Admin/Controllers/CoverTypeController.cs
@@ -5,12 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace BulkyBook.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class CategoryController : Controller
+    public class CoverTypeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
 
-
-        public CategoryController(IUnitOfWork unitOfWork)
+        public CoverTypeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
@@ -22,68 +21,70 @@ namespace BulkyBook.Areas.Admin.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            Category category = new Category();
+            CoverType coverType = new CoverType();
             if (id == null)
             {
                 // CREATE
-                return View(category);
+                return View(coverType);
             }
-            // EDIT
-            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
-            // if supplied incorrect ID
-            if (category == null)
+            // EDIT 
+            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            // if incorrect ID 
+            if (coverType == null)
             {
                 return NotFound();
             }
 
-            return View(category);
+            return View(coverType);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Category category)
+        public IActionResult Upsert(CoverType coverType)
         {
             if (ModelState.IsValid)
             {
                 // CREATE
-                if (category.Id == 0)
+                if (coverType.Id == 0)
                 {
-                    _unitOfWork.Category.Add(category);
+                    _unitOfWork.CoverType.Add(coverType);
                 }
                 else
                 {
-                    // U UPDATE CATEGORY REPA METODI POZIVAMO SAVECHANGES / REMOVE-ALI SMO TO , DA BUDEMO CONSISTENT
-                    _unitOfWork.Category.Update(category);
+                    // Update
+                    _unitOfWork.CoverType.Update(coverType);
                 }
 
                 _unitOfWork.Save();
-                // DA BI IZBJEGLI MAGIC STRING VRACANJE
-                // return RedirectToAction("Index");
+
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(category);
+            // ako je invalid ModelState vračamo isti view
+            return View(coverType);
         }
 
-        #region API 
+        #region API Calls
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Category.GetAll();
+            var allObj = _unitOfWork.CoverType.GetAll();
             return Json(new { data = allObj });
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.Category.Get(id);
+            var objFromDb = _unitOfWork.CoverType.Get(id);
             if (objFromDb == null)
             {
-                return Json(new { success = false, message = "Error while deleteing" });
+                return Json(new { success = false, message = "Error while deleting" });
             }
-            _unitOfWork.Category.Remove(objFromDb);
+
+            _unitOfWork.CoverType.Remove(objFromDb);
             _unitOfWork.Save();
-            return Json(new { success = true, message = "Delete Successful" });
+
+            return Json(new { success = true, message = "Delete successful" });
         }
         #endregion
     }
